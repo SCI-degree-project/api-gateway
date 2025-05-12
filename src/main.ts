b76e0e52e470c7d6d047980b0edcef3f9ff9e1e6
Leonardo Herrera <leonardoherrera2304@gmail.com>
2025-05-12 18:59:34 -0400

    feat: frontend cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AllExceptionsFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(7777);
